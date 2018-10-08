c9893aa3e54103a24d27a636f8b9cbdcf3e0a547
AyoubeAkaouch <ayoube.akaouch@gmail.com>
2018-10-08 16:00:46 -0400

    Started coding the region implementtation.

--- KingOfNewYork/Region.cpp
+++ KingOfNewYork/Region.cpp
@@ -6,8 +6,27 @@ Region::Region()
 {
 }
 
+Region::Region(GraphGeneric<SubRegion> subRegion)
+{
+	subRegions.push_back(subRegion);
+}
+
 
 Region::~Region()
 {
+}
+
+vector<string> Region::getOwners()
+{
+	if (currentOwners.size == 0) {
+		cout << "There are no players currently in this region. Vector is empty." << endl;
+	}
+	
+	return currentOwners;
+}
+
+void Region::setOwner(string player)
+{
+	this->currentOwners.push_back(player);
 }
 
--- KingOfNewYork/Region.h
+++ KingOfNewYork/Region.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "SubRegion.h"
+#include "GraphGeneric.h"
 
 using namespace std;
 
@@ -7,9 +8,13 @@ class Region
 {
 public:
 	Region();
+	Region(GraphGeneric<SubRegion> subRegion);
 	~Region();
+	vector<string> getOwners();// Change for player object
+	void setOwner(string player);// Take player object after
 private:
-	
+	vector<GraphGeneric<SubRegion>> subRegions;
+	vector<string> currentOwners;// Change for player object after
+
+	int maxPlayers;
 };
-
-
