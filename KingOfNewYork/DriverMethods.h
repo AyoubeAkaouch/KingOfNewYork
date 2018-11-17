2fe3389783257f6ac27b92f5e58de8b5cf1c1354
AyoubeAkaouch <ayoube.akaouch@gmail.com>
2018-11-17 11:14:43 -0500

    Removed some methods that were moved. - Removed the methods in driveerMethods since they are now handled by the strategy pattern.

--- KingOfNewYork/DriverMethods.h
+++ KingOfNewYork/DriverMethods.h
@@ -15,10 +15,4 @@ using namespace std;
  void settingRegions(GameMap& gameMap, vector<Player*>& players, int currentTurn);
  void setPlayers(vector<Player*>& players, vector<Monster>& monsters,StrategyPlayerInterface* strategy);
  void gameLoop(vector<Player*>& players, GameMap& gameMap, EffectCardDeck& effectCards, BuildingTilesDeck& tilesDeck, map<string, vector<Token>>& tokens,int firstToPlay);
- void diceRoll(Player& player, bool extraDices);
- void resolveDices(Player& player, GameMap& gameMap, vector<Player*> & players);
- void move(Player& player, GameMap& gameMap, bool gotAttacked);
- void buyCards(Player& player,vector<EffectCard>& buyableCards,EffectCardDeck& effectCards);
- vector<int> stringToVectorInt(string reRolls); // To help with parsing input when selecting rerolls
- void applyDiceEffect(vector<string> effect, Player& player, GameMap& gameMap, vector<Player*> & players);
  bool gameEnded(vector<Player*>& players, GameMap& gameMap);// In this one we remove all players that are at 0 health and check if winning condition achieved!
